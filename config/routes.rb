ed905bbe0e9ff963deaec52f7443c388c145e65d
Kirill <ushnurtsev@gmail.com>
2014-04-02 19:14:19 +0400

    Finish static

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SampleApp::Application.routes.draw do
   get "static_pages/home"
   get "static_pages/help"
+  get "static_pages/about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -16,7 +17,7 @@ SampleApp::Application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
-  # Example resource route with options:
+  # Example resource route with options:3lnWkQ8kMt
   #   resources :products do
   #     member do
   #       get 'short'
